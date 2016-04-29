54c8f8861b69a21521a73dd4849e086d385eb1ea
Taylor Sargent <sarg0049@algonquinlive.com>
2016-04-29 08:03:51 -0400

    fixed spaces in js file

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@ var $fruitani = $('.fruit-ani');
 var $chartani = $('.chart-ani');
 
 $fruitani.waypoint(function (direction) {
-  if (direction == 'down'){
+  if (direction == 'down') {
     $fruitani.addClass('js-svg-animate');
   } else {
-  $fruitani.removeClass('js-svg-animate');
+    $fruitani.removeClass('js-svg-animate');
   }
-}, { offset: '50%' });
+}, {offset: '50%'});
 
 $chartani.waypoint(function (direction) {
-  if (direction == 'down'){
+  if (direction == 'down') {
     $chartani.addClass('js-svg-animate-2');
   } else {
-  $chartani.removeClass('js-svg-animate-2');
+    $chartani.removeClass('js-svg-animate-2');
   }
-}, { offset: '10%' });
+}, {offset: '10%'});
